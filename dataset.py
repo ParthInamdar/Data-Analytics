38917b8d87035a01f0eed0afe1805af3dabe4161
parth <parthinamdar218@gmail.com>
2026-06-01 13:38:46 +0530

    initial commit

--- dataset.py
+++ dataset.py
@@ -1,6 +1,23 @@
+# Customer Churn & Revenue Risk Analysis
+
+#----------------------------------------------------------------------------#
+
+"""Business Problem
+A telecom company is losing customers.
+Analyze customer behavior, identify churn drivers, estimate revenue at risk,
+and provide retention recommendations."""
+
+#----------------------------------------------------------------------------#
+
+"""Analyzed 7,000+ telecom customers,
+identified contract type, tenure, and monthly charges as key churn drivers,
+and proposed retention strategies that could reduce customer loss."""
+
+#----------------------------------------------------------------------------#
+
+
 import pandas as pd
 
-df = pd.read_csv("Amazon.csv")
+df = pd.read_csv("Telco-Customer-Churn.csv")
 
-print(df.info())
-print(df.head(2))
+print(df[df["gender"]== "Female"])
